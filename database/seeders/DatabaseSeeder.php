06117e2030ebcd3f3a3e9244cce57998d2984693
jimdmnc <jmdmnc@gmail.com>
2025-03-10 07:44:06 +0800

    seed account for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,9 +9,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Dominic Pabalate',
-            'email' => 'pabalatedominic2@gmail.com', // Change to your actual admin email
-            'password' => Hash::make('dominic123'), // ✅ Hash the password correctly
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com', // Change to your actual admin email
+            'password' => Hash::make('admin123'), // ✅ Hash the password correctly
             'role' => 'admin', // Ensure your 'users' table has a 'role' column
         ]);
     }
