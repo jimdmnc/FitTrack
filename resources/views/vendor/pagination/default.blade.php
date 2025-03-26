b22adcf407ca74de2e7019c5cee3728993a028ab
eulicemage05 <eulice.mage57@gmail.com>
2025-03-26 16:27:56 +0800

    fix design

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,15 +1,15 @@
 @if ($paginator->hasPages())
     <nav class="flex items-center justify-center my-6" aria-label="Pagination">
-        <div class="flex items-center rounded-lg shadow-sm bg-white">
+        <div class="flex items-center rounded-lg shadow-sm">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <span class="inline-flex items-center justify-center w-10 h-10 text-gray-300 cursor-not-allowed" aria-disabled="true">
+                <span class="inline-flex items-center justify-center w-10 h-10 text-gray-200 cursor-not-allowed" aria-disabled="true">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                     </svg>
                 </span>
             @else
-                <a href="{{ $paginator->previousPageUrl() }}" class="inline-flex items-center justify-center w-10 h-10 text-gray-500 transition-colors duration-150 hover:text-blue-600" rel="prev" aria-label="@lang('pagination.previous')">
+                <a href="{{ $paginator->previousPageUrl() }}" class="inline-flex items-center justify-center w-10 h-10 text-gray-200 transition-colors duration-150 hover:text-[#ff5722]" rel="prev" aria-label="@lang('pagination.previous')">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                     </svg>
@@ -21,20 +21,20 @@
                 @foreach ($elements as $element)
                     {{-- "Three Dots" Separator --}}
                     @if (is_string($element))
-                        <span class="px-1 text-gray-400">{{ $element }}</span>
+                        <span class="px-1 text-gray-200">{{ $element }}</span>
                     @endif
 
                     {{-- Array Of Links --}}
                     @if (is_array($element))
                         @foreach ($element as $page => $url)
                             @if ($page == $paginator->currentPage())
-                                <span class="inline-flex items-center justify-center w-8 h-8 mx-1 text-white bg-blue-600 rounded-full" aria-current="page">
+                                <span class="inline-flex items-center justify-center w-8 h-8 mx-1 text-white bg-[#ff5722] rounded-full" aria-current="page">
                                     {{ $page }}
                                 </span>
                             @else
-                                <a href="{{ $url }}" class="inline-flex items-center justify-center w-8 h-8 mx-1 text-gray-600 transition-colors duration-150 rounded-full hover:bg-gray-100">
-                                    {{ $page }}
-                                </a>
+                            <a href="{{ $url }}" class="inline-flex items-center justify-center w-8 h-8 mx-1 text-gray-200 transition-transform duration-150 rounded-full hover:bg-[#ff5722] hover:scale-110">
+                                {{ $page }}
+                            </a>
                             @endif
                         @endforeach
                     @endif
@@ -48,7 +48,7 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}" class="inline-flex items-center justify-center w-10 h-10 text-gray-500 transition-colors duration-150 hover:text-blue-600" rel="next" aria-label="@lang('pagination.next')">
+                <a href="{{ $paginator->nextPageUrl() }}" class="inline-flex items-center justify-center w-10 h-10 text-gray-200 transition-colors duration-150 hover:text-[#ff5722]" rel="next" aria-label="@lang('pagination.next')">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                     </svg>
