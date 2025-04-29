2ac8e4632c4e9f3e15667268ee78ef58bf72963e
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-04-30 01:02:13 +0800

    sdsd

--- resources/views/self/landing.blade.php
+++ resources/views/self/landing.blade.php
@@ -258,7 +258,7 @@
             </a>
             
             <!-- Google Play Store Button -->
-            <a href="#" class="bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 rounded-lg inline-flex items-center text-sm md:text-base transition duration-300 shadow-lg">
+            <a href="https://play.google.com/store/apps/details?id=com.FitTrack.fittrackapp&pcampaignid=web_share" class="bg-black hover:bg-gray-800 text-white font-bold py-3 px-6 rounded-lg inline-flex items-center text-sm md:text-base transition duration-300 shadow-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 512 512">
                     <path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.6 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/>
                 </svg>
