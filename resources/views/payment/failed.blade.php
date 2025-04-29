f7f64f7779834dc5e41b18f45ad2cc099a425b8e
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-04-29 23:59:44 +0800

    sdsdd

---
+++ resources/views/payment/failed.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Payment Failed</h1>
+        <p>Unfortunately, your payment was not successful. Please try again.</p>
+    </div>
+@endsection
