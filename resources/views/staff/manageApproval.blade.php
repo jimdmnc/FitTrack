72cc3452d53fd3f11d7ccc28ad0a6a2c6b8a81c2
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-02 23:50:52 +0800

    sdsd

--- resources/views/staff/manageApproval.blade.php
+++ resources/views/staff/manageApproval.blade.php
@@ -117,14 +117,14 @@
                 @endif
                 </td>
                 <td class="p-3 font-medium text-gray-200">
-                    @if($payment && $payment->payment_method == 'gcash')
-                        <span class="px-2 py-1 rounded-full text-xs font-semibold bg-indigo-500 text-white flex items-center">
+                @if($user->payment && $user->payment->payment_method == 'gcash')
+                <span class="px-2 py-1 rounded-full text-xs font-semibold bg-indigo-500 text-white flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.31-8.86c-1.77-.45-2.34-.94-2.34-1.67 0-.84.79-1.43 2.1-1.43 1.38 0 1.9.66 1.94 1.64h1.71c-.05-1.34-.87-2.57-2.49-2.97V5H10.9v1.69c-1.51.32-2.72 1.3-2.72 2.81 0 1.79 1.49 2.69 3.66 3.21 1.95.46 2.34 1.15 2.34 1.87 0 .53-.39 1.39-2.1 1.39-1.6 0-2.23-.72-2.32-1.64H8.04c.1 1.7 1.36 2.66 2.86 2.97V19h2.34v-1.67c1.52-.29 2.72-1.16 2.73-2.77-.01-2.2-1.9-2.96-3.66-3.42z"/>
                             </svg>
                             GCash
                         </span>
-                    @elseif($payment && $payment->payment_method == 'cash')
+                        @elseif($user->payment && $user->payment->payment_method == 'cash')
                         <span class="px-2 py-1 rounded-full text-xs font-semibold bg-yellow-500 text-black flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 mr-1" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"/>
