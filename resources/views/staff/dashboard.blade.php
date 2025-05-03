cb252e37e58c1c1e1772b48b90145c6bdd258c2e
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-03 17:48:38 +0800

    sdsdsds

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -1037,14 +1037,7 @@ document.addEventListener("DOMContentLoaded", function () {
             ]
         },
         options: {
-            layout: {
-        padding: {
-            left: 20,
-            right: 20,
-            top: 20,
-            bottom: 20
-        }
-    }
+            
             responsive: true,
             maintainAspectRatio: false,
             interaction: {
