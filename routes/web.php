ef27271b7968cfd211e6aba2f80c94719dc4429a
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-04 21:49:22 +0800

    sdsdsd

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\RFIDController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Staff\DashboardController;
