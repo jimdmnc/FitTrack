c3293f0cd49d847b2e69b4db39d363e35c8b10ad
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-12 21:39:19 +0800

    sdsd

--- routes/web.php
+++ routes/web.php
@@ -155,12 +155,11 @@ Route::middleware('auth')->group(function () {
 
 
 
-    Route::resource('announcements', AnnouncementController::class);
-// Dashboard route
-Route::get('dashboard', function () {
-    $announcements = Announcement::latest()->get();
-    return view('dashboard', compact('announcements'));
-})->name('dashboard');
+        Route::get('dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
+        Route::resource('announcements', AnnouncementController::class);
     
 
 });
