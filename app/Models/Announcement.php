818590d89a8d9ecce1c79e06c146bd6bd86898c9
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-12 21:34:05 +0800

    sdsdsd

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -10,4 +10,8 @@ class Announcement extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'content', 'schedule', 'type'];
+
+    protected $casts = [
+        'schedule' => 'datetime',
+    ];
 }
