a62d178c9a32bc799002f8df2f2fe5cfe6e5cd3b
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-18 09:46:47 +0800

    sdsdd

--- app/Http/Controllers/SelfRegistrationController.php
+++ app/Http/Controllers/SelfRegistrationController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Attendance;
+use App\Models\Price;
 
 class SelfRegistrationController extends Controller
 {
