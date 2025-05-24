e0d2ebea9230206d58f6f6db7d7b338db8f9cb5a
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-25 07:03:13 +0800

    sdssd

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'first_name' => 'Test',
+            'last_name' => 'Admin',
+            'gender' => 'male',
+            'phone_number' => '09171234567',
+            'membership_type' => 'staff',
+            'start_date' => now()->toDateString(),
+            'rfid_uid' => 'STAFF1234',
+            'email' => 'testadmin@gmail.com',
+            'birthdate' => '1990-01-01',
+            'email_verified_at' => now(),
+            'password' => Hash::make('admin123'),
+
+            'role' => 'super_admin',
+            'member_status' => 'active',
+            'revoke_reason' => null,
+            'revoked_at' => null,
+            'session_status' => 'pending',
+            'rejection_reason' => null,
+            'needs_approval' => 0,
+        ]);
     }
 }
