09f43fabd3eddcb41b679f7dd93931c4f1d00387
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-25 09:42:47 +0800

    sdsdd

--- routes/web.php
+++ routes/web.php
@@ -153,7 +153,7 @@ Route::middleware('auth')->group(function () {
 
     // Announcements
     Route::resource('announcements', AnnouncementController::class);
-    Route::get('/landing-profile', [AnnouncementController::class, 'index'])->name('self.landingProfile');
+    Route::get('/landing-profile', [AnnouncementController::class, 'landing'])->name('self.landingProfile');
 });
 
 // Authentication routes (login, register, etc.)
