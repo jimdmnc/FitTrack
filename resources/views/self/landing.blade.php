7eb670ae6b11d5ba11d802a9dc7be110c558ed36
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-26 21:42:59 +0800

    sdsdsdd

--- resources/views/self/landing.blade.php
+++ resources/views/self/landing.blade.php
@@ -170,6 +170,9 @@
             <a href="#home" class="block py-2 text-center hover:bg-gray-800 rounded">Home</a>
             <a href="#tutorial" class="block py-2 text-center hover:bg-gray-800 rounded">Tutorial</a>
             <a href="#inhere" class="block py-2 text-center hover:bg-gray-800 rounded">In Here</a>
+        </div>
+
+        <div class="flex items-center">
             <a href="{{ route('self.login') }}" class="block py-2 text-center bg-gradient-to-r from-orange-600 to-orange-700 text-white rounded hover:from-orange-700 hover:to-orange-800 transition duration-300">Login</a>
         </div>
     </nav>
