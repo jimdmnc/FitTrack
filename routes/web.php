e2692d10e567303a7d417700018ec82452047a96
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-05-28 22:33:36 +0800

    dsdsd

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/landing', function () {
 })->name('self.landing');
 
 // Self-registration routes
-Route::get('session-registration', [SelfRegistrationController::class, 'index']);
+Route::get('session-registration', [SelfRegistrationController::class, 'index'])->name('self.registration');
 Route::post('session-registration', [SelfRegistrationController::class, 'store'])->name('self.registration.store');
 Route::post('membership-renewal', [SelfRegistrationController::class, 'renew'])->name('self.membership.renew');
 Route::get('/attendance/status', [SelfRegistrationController::class, 'checkAttendanceStatus'])->name('self.checkAttendanceStatus');
