b258bf00dcfb9d74bb29d16de552fa1bb42646e8
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-06-01 10:14:29 +0800

    sdsdsd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,12 +40,12 @@ class AuthController extends Controller
                 ], 403);
             }
             
-            if ($user->role === 'userSession') {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Not Regular members are not allowed on this app'
-                ], 403);
-            }
+            // if ($user->role === 'userSession') {
+            //     return response()->json([
+            //         'success' => false,
+            //         'message' => 'Not Regular members are not allowed on this app'
+            //     ], 403);
+            // }
     
             if (!$user->rfid_uid) {
                 return response()->json([
