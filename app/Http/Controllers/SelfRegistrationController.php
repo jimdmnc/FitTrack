6ffdb5ab3e6c4f74e5ee3c6356048427615c1399
jimdmnc <133569265+jimdmnc@users.noreply.github.com>
2025-06-01 22:01:10 +0800

    sdsdd

--- app/Http/Controllers/SelfRegistrationController.php
+++ app/Http/Controllers/SelfRegistrationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\MembersPayment;
+use App\Models\Announcement;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
