afe21bcfa7c5d5a1eada49b77765d85c71e7c7ba
jimdmnc <pabalatedominic4@gmail.com>
2025-07-01 09:07:04 +0800

    sdsdd

--- resources/views/staff/attendance.blade.php
+++ resources/views/staff/attendance.blade.php
@@ -116,6 +116,8 @@
 
     <div class="p-4">
         <div class="flex flex-col sm:flex-row items-start sm:items-center justify-between px-3 sm:px-6 py-4 border-b border-gray-800 gap-4">
+        <div class="flex flex-wrap gap-2 w-full sm:w-auto">
+
             <form method="GET" action="{{ route('staff.attendance.index') }}" class="w-full sm:w-64 md:w-80">
                 <div class="relative flex items-center -ml-4 mb-2">
                     <!-- Search Input -->
@@ -151,7 +153,10 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                     </svg>
                     Refresh
-                </button>
+            </button>
+    </div>
+
+
             <div class="relative w-full sm:w-auto">
                 <button id="select-btn" class="w-full px-6 py-2 text-gray-200 bg-[#212121] border border-[#666666] hover:border-[#ff5722] rounded-lg flex justify-between items-center">
                     <span id="selected-option">today</span>
