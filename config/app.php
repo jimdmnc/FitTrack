ae22879165bd93ee6f05087932359d9df0146ae9
jimdmnc <pabalatedominic4@gmail.com>
2025-08-29 10:13:21 +0800

    sdsdd

--- config/app.php
+++ config/app.php
@@ -2,45 +2,6 @@
 
 return [
 
-
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Class Aliases
-    |--------------------------------------------------------------------------
-    |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
-    |
-    */
-
-    'aliases' => [
-
- 
-
-        // ✅ Add Firebase alias here
-        'Firebase' => Kreait\Laravel\Firebase\Facades\Firebase::class,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    */
-
-    'providers' => [
-
-        /*
-         * Package Service Providers...
-         */
-        Kreait\Laravel\Firebase\ServiceProvider::class, // ✅ Add this
-
-    ],
-
-
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
