281137e2f5eb82eac373b9763972e8ac55c774e9
jimdmnc <pabalatedominic4@gmail.com>
2025-08-29 10:00:54 +0800

    sdsdsd

--- app/Services/FirebaseNotificationService.php
+++ app/Services/FirebaseNotificationService.php
@@ -2,9 +2,7 @@
 
 namespace App\Services;
 
-use Kreait\Laravel\Firebase\Facades\Firebase;
 use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification;
 use Kreait\Firebase\Exception\MessagingException;
 use Illuminate\Support\Facades\Log;
 
@@ -13,12 +11,16 @@ class FirebaseNotificationService
     public function sendNotification(string $title, string $body, array $fcmTokens, array $data = [])
     {
         try {
-            $messaging = Firebase::messaging();
+            $messaging = app('firebase.messaging');
 
             foreach ($fcmTokens as $token) {
-                $message = CloudMessage::withTarget('token', $token)
-                    ->withNotification(Notification::create($title, $body))
-                    ->withData($data);
+                $message = CloudMessage::new()
+                    ->withNotification([
+                        'title' => $title,
+                        'body' => $body,
+                    ])
+                    ->withData($data)
+                    ->withTarget('token', $token);
 
                 $response = $messaging->send($message);
 
