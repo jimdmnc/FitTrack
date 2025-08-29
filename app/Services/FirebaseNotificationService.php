d1e8feb4dac8615cc0feb82a9725557c170985a5
jimdmnc <pabalatedominic4@gmail.com>
2025-08-29 10:37:30 +0800

    sdsdds

--- app/Services/FirebaseNotificationService.php
+++ app/Services/FirebaseNotificationService.php
@@ -2,27 +2,37 @@
 
 namespace App\Services;
 
+use Kreait\Firebase\Factory;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Exception\MessagingException;
 use Illuminate\Support\Facades\Log;
 
 class FirebaseNotificationService
 {
+    private $messaging;
+
+    public function __construct()
+    {
+        // Point to your Firebase service account JSON key
+        $serviceAccountPath = base_path('firebase-key.json'); 
+        // Upload your service account file to Laravel root folder and rename it firebase-key.json
+
+        $factory = (new Factory)->withServiceAccount($serviceAccountPath);
+        $this->messaging = $factory->createMessaging();
+    }
+
     public function sendNotification(string $title, string $body, array $fcmTokens, array $data = [])
     {
         try {
-            $messaging = app('firebase.messaging');
-
             foreach ($fcmTokens as $token) {
-                $message = CloudMessage::new()
+                $message = CloudMessage::withTarget('token', $token)
                     ->withNotification([
                         'title' => $title,
                         'body' => $body,
                     ])
-                    ->withData($data)
-                    ->withTarget('token', $token);
+                    ->withData($data);
 
-                $response = $messaging->send($message);
+                $response = $this->messaging->send($message);
 
                 Log::info('Firebase notification sent successfully', [
                     'token' => $token,
